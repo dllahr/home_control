88f7ffe83d0c934f04da1bd69c57d6b6581c8953
Dave Lahr <github.20.dllahr@xoxy.net>
2016-11-27 15:47:22 -0500

    server.js:  extend to allow addition of thermostat on/off data

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ var db = new sqlite3.Database('/media/raid/dllahr/projects/temperatureMonitoring
 //This query returns the temperature data (measurement_type_id=1) for 
 //the most recently recorded week (1 week = 604800 s, 3 devices, 3 measurements = approximately 5443200 entries)
 //at 10 minute intervals
-var query = "select device_id, time, value from measurements "
-query += " where id > (select max(id)-5443200 from measurements) "
-query += " and measurement_type_id=1 and time%600 = 0"
+var query = 'select device_id, time, value from measurements '
+query += ' where id > (select max(id)-5443200 from measurements) '
+query += ' and measurement_type_id=1 and time%600 = 0';
 
+var insertStmtStr = 'insert into measurements (device_id, time, measurement_type_id, value, measurement_unit_id) values (?, ?, ?, ?, ?)';
 
 var my_err_handler = function(err) {
 	if (err) {
@@ -19,49 +20,59 @@ var my_err_handler = function(err) {
 
 
 http.createServer(function (request, response) {
-	if (request.method == "GET") {
+	if (request.method == 'GET') {
 //		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.writeHead(200, {"Content-Type":"application/json"});
+		response.writeHead(200, {'Content-Type':'application/json'});
 
 		db.all(query, function(err, rows) {
 			if (err) {
 				my_err_handler(err);
 			} else {
-				/*var body = "";
+				/*var body = '';
 				for (var i = 0; i < rows.length; i++) {
-					body += rows[i].device_id + "\t" + rows[i].time + "\t" + rows[i].value + "\n";
+					body += rows[i].device_id + '\t' + rows[i].time + '\t' + rows[i].value + '\n';
 				}
 				response.end('Hello the Alka Bear and Beary the Polar Bear\n' + body);*/
 				response.end(JSON.stringify(rows));
 			}
 		});
 
-	} else if (request.method == "POST") {
+	} else if (request.method == 'POST') {
 		var requestBody = '';
 		request.on('data', function(data) {
 			requestBody += data;
 		});
 
 		request.on('end', function() {
-			var formData = JSON.parse(requestBody);
-			var now = new Date();
-			console.log('data received from formData.deviceId:  ' + formData.deviceId + ' at now:  ' + now);
 
-			var stmt = db.prepare('insert into measurements (device_id, time, measurement_type_id, value, measurement_unit_id) values (?, ?, ?, ?, ?)');
+			var formData = JSON.parse(requestBody);
+			var deviceId = formData.deviceId;
+
+			var now = new Date();
+			console.log('data received from deviceId:  ' + deviceId + ' at now:  ' + now);
+
+			var stmt = db.prepare(insertStmtStr);
 			
 			for (var i = 0; i < formData.readTimes.length; i++) {
 				var t = formData.readTimes[i];
 
 				var TF = formData.temperaturesF[i];
-				stmt.run(formData.deviceId, t, 1, TF, 1, my_err_handler);
+				stmt.run(deviceId, t, 1, TF, 1, my_err_handler);
 				
 				var ll = formData.lightLevels[i];
-				stmt.run(formData.deviceId, t, 2, ll, 2, my_err_handler);
+				stmt.run(deviceId, t, 2, ll, 2, my_err_handler);
 
 				var hwv = formData.hardwareVoltages[i];
-				stmt.run(formData.deviceId, t, 3, hwv, 3, my_err_handler);
+				stmt.run(deviceId, t, 3, hwv, 3, my_err_handler);
 			}
 			stmt.finalize();
+
+			if ('thermostatOnOff' in formData && formData['thermostatOnOff'] != null) {
+				var mostRecentTime = formData.readTimes.slice(-1)[0];
+				var value = formData.thermostatOnOff;
+				console.log('adding thermostatOnOff entry value:  ' + value + ' typeof(value):  ' + typeof(value));
+				db.run(insertStmtStr, deviceId, mostRecentTime, 4, value, 4);
+			}
 		});
 	}
 }).listen(8124);
