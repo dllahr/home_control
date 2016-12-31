50122800cfc06b3f7c156d9f024c9202c12b98f5
Dave Lahr <github.20.dllahr@xoxy.net>
2016-12-31 11:35:58 -0500

    database/add_devices.sql:  added some devices

--- database/add_devices.sql
+++ database/add_devices.sql
@@ -1,3 +1,6 @@
-insert into device(id) values ('2371a1aa5ba5ceee');
-insert into device(id) values ('233db2aa5ba5ceee');
-insert into device(id) values ('23133eaa5ba5ceee');
+insert into device(id) values ("2371a1aa5ba5ceee");
+insert into device(id) values ("233db2aa5ba5ceee");
+insert into device(id) values ("23133eaa5ba5ceee");
+insert into device(id) values ("23576cab5ba5ceee");
+insert into device(id) values ("237da4aa5ba5ceee");
+
